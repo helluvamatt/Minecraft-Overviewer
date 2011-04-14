c7fe773c88410a297750ad8b5406132e5862c2ad
Fabian Norman <FabianN@gmail.com>
2011-04-14 00:01:33 -0700

    Added MapMarker's support for bukkit.

---
+++ web_assets/mapmarkers/com/MapMarkers/TJ09/MapMarkersPlayerListener.java
@@ -0,0 +1,37 @@
+package com.MapMarkers.TJ09;
+
+import java.util.Date;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerListener;
+
+/**
+ * Handle events for all Player related events
+ * @author TJ09
+ */
+public class MapMarkersPlayerListener extends PlayerListener {
+    private final MapMarkers plugin;
+
+    public MapMarkersPlayerListener(MapMarkers instance) {
+        plugin = instance;
+    }
+	
+	public void onPlayerMove(PlayerMoveEvent event) {
+		plugin.updatePlayer(event.getPlayer());
+	}
+	
+	public void onPlayerLogin(PlayerLoginEvent event) {
+		plugin.updatePlayer(event.getPlayer());
+	}
+	
+	public void onPlayerTeleport(PlayerMoveEvent event) {
+		plugin.updatePlayer(event.getPlayer());
+	}
+	
+	public void onPlayerQuit(PlayerEvent event) {
+		plugin.removePlayer(event.getPlayer());
+	}
+}
+
